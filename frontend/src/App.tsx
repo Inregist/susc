510e36d6b8968e426e5697c80bdc4ce98aa30cad
inregist <inregist.irg@gmail.com>
2020-10-28 16:06:20 +0700

    add antd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./styles/tailwind.css";
 import "./styles/style.css";
+import "antd/dist/antd.css";
+import { Button } from "antd";
 
 const App = () => {
   return <div></div>;
--- office/src/App.tsx
+++ office/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles/style.css";
 import "./styles/tailwind.css";
+import "antd/dist/antd.css";
 
 const App = () => {
   return <div></div>;
