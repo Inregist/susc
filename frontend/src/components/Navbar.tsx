f4ad94a825c8bc3d3d5fbd913b9302f03ea5dd10
inregist <inregist.irg@gmail.com>
2020-11-14 18:26:09 +0700

    fix home

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,18 +2,23 @@ import React from "react";
 import home_bt from "../assets/home.png";
 import back_prev from "../assets/left-arrow.png";
 
-const MainNavbar = ({ name }) => {
+const Navbar = ({ name }) => {
   return (
-    <div className="w-full bg-blue-900 text-white text-xl h-16 flex items-center justify-between p-4">
-      <a href="http://localhost:3000">
-        <img src={back_prev} alt="back" className="mt-1 w-6 h-6" />
-      </a>
-      {name}
-      <a href="http://localhost:3000">
-        <img src={home_bt} alt="home" className="w-8 h-8" />
-      </a>
-    </div>
+    <>
+      <div>
+        <div className="fixed w-full bg-blue-900 text-white text-xl h-16 flex items-center justify-between p-4">
+          <a href="http://localhost:3000">
+            <img src={back_prev} alt="back" className="mt-1 w-6 h-6" />
+          </a>
+          {name}
+          <a href="http://localhost:3000">
+            <img src={home_bt} alt="home" className="w-8 h-8" />
+          </a>
+        </div>
+      </div>
+      <div className="h-16"></div>
+    </>
   );
 };
 
-export default MainNavbar;
+export default Navbar;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,43 +1,41 @@
 import React from "react";
-import Navbar from "../components/MainNavbar";
 import Search from "../assets/Search.png";
 import Report from "../assets/Report.png";
 import Statistic from "../assets/Statistic.png";
 import CheckCase from "../assets/CheckCase.png";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    <div>
-      <Navbar/>
-
-      <div className="bg-gray-300 p-4 h-screen">
-        <a href="./search/" className="mb-2 flex justify-center">
-          {/* <div className="absolute bottom-0 w-full flex justify-center pb-12">
+    <div className="h-screen">
+    <Navbar name="Home" />
+        <div className="p-4">
+          <a href="./search/" className="mb-2 flex justify-center">
+            {/* <div className="absolute bottom-0 w-full flex justify-center pb-12">
             <div className="w-2/3 h-8 bg-white flex items-center">
-              <div className="bg-blue-900 h-full px-2 text-white leading-8 rounded-l-md truncate">
-                ค้นหา
-              </div>
-              <input className="" type="text" />
+            <div className="bg-blue-900 h-full px-2 text-white leading-8 rounded-l-md truncate">
+            ค้นหา
+            </div>
+            <input className="" type="text" />
             </div>
           </div> */}
-          <img src={Search} alt="checking" />
-        </a>
-
-        <a href="./report-case/" className="flex justify-center mb-2">
-          <img src={Report} alt="report-case" />
-        </a>
-
-        <div className="flex justify-center">
-          <a href="./statistic/">
-            <img src={Statistic} alt="statistic" />
+            <img src={Search} alt="checking" />
           </a>
-          <a href="./check-case/">
-            <img src={CheckCase} alt="check-case" />
+
+          <a href="./report-case/" className="flex justify-center mb-2">
+            <img src={Report} alt="report-case" />
           </a>
+
+          <div className="flex justify-center">
+            <a href="./statistic/">
+              <img src={Statistic} alt="statistic" />
+            </a>
+            <a href="./check-case/">
+              <img src={CheckCase} alt="check-case" />
+            </a>
+          </div>
         </div>
-        
       </div>
-    </div>
   );
 };
 
