bbf2e09af0e5de7e6e31fb5be6fa7058f0e4b991
Garakate <hwkate@hotmail.co.th>
2020-11-17 05:29:32 +0700

    EditTimePicker

--- office/src/pages/AddTime.tsx
+++ office/src/pages/AddTime.tsx
@@ -17,20 +17,21 @@ const AddTime = () => {
         setShow(false);
       }}
     >
-      <div className="md:font-bold  h-10 text-lg flex w-full ">
+      <div className="md:font-bold  h-10 text-lg flex w-full flex justify-around  border-b border-gray-500 p-0.5">
         เพิ่มเวลานัดหมาย
       </div>
-      <div className="p-4">
+      <div className="pt-12 pb-4 pl-10 pr-4 flex justify-center ">
         <DatePicker
           selected={startDate}
           onChange={(date) => setStartDate(date)}
           timeInputLabel="Time:"
           dateFormat="MM/dd/yyyy h:mm aa"
           showTimeInput
+          
         />
       </div>
 
-      <div className="flex justify-start  border-t border-gray-500 pt-8 pb-px pl-4 pr-4">
+      <div className=" flex justify-center pt-8 pb-px pl-4 pr-4">
         <button
           className="bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded "
           onClick={() => {
