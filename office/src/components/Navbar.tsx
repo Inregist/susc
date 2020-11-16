92e59cfa586f48b1fe0ec40c39dfb8af5c276a50
Tomato-Cultivator <Tomato-Cultivator@users.noreply.github.com>
2020-11-16 21:30:14 +0700

    Navbar

--- office/src/components/Navbar.tsx
+++ office/src/components/Navbar.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Navbar = () => {
-  return <div></div>;
+const Navbar = ({name}) => {
+  return <div className="w-full bg-blue-900 text-white text-xl h-16 flex items-center justify-between p-4">
+    <div className="ml-5">
+      {name}
+    </div>
+    <div className="mr-10 ">
+      นายทำงาน หนักมาก
+    </div>
+  </div>;
 };
 
 export default Navbar;
---
+++ office/src/pages/UpdateCase.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Modal, Button } from 'antd';
+
+const UpdateCase = () => {
+    return(
+        <div >
+            <Navbar name="ระบบจัดการข้อมูล"/>
+        </div>
+    );
+
+};
+
+export default UpdateCase;
