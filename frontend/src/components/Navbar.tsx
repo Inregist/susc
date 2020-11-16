17a664d3412106326a469cc493053f56d106d54a
NatthakitD <n.darapongsathaporn@gmail.com>
2020-11-16 22:05:15 +0700

    fixed navbar frontend

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -7,11 +7,11 @@ const Navbar = ({ name }) => {
     <>
       <div>
         <div className="w-full bg-blue-900 text-white text-xl h-16 flex items-center justify-between p-4">
-          <a href="http://localhost:3000">
+          <a href="/Home">
             <img src={back_prev} alt="back" className="mt-1 w-6 h-6" />
           </a>
           {name}
-          <a href="http://localhost:3000">
+          <a href="/Home">
             <img src={home_bt} alt="home" className="w-8 h-8" />
           </a>
         </div>
