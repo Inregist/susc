cf628e0e2f5ac38987060c5daea26d6c06afcd64
inregist <inregist.irg@gmail.com>
2020-11-16 17:16:47 +0700

    fix dropdown & clean code

--- frontend/src/pages/Statistic.tsx
+++ frontend/src/pages/Statistic.tsx
@@ -1,19 +1,28 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import { Pie } from "react-chartjs-2";
-import {
-  Menu,
-  Dropdown,
-  Row,
-  Col,
-  Divider,
-  Typography,
-  Checkbox,
-  Tabs,
-} from "antd";
+import { Menu, Dropdown, Checkbox, Tabs } from "antd";
 import { DownOutlined } from "@ant-design/icons";
 
 const Statistic = () => {
+  const [showCat, setShowCat] = useState(false);
+  const [showPlatform, setShowPlatform] = useState(false);
+
+  const [selectedCat, setSelectedCat] = useState([
+    { name: "เครื่องสำอาง", check: false },
+    { name: "เสื้อผ้า", check: false },
+    { name: "เครื่องใช้ไฟฟ้า", check: false },
+    { name: "ของใช้ในครัว", check: false },
+    { name: "ของใช้ในสำนักงาน", check: false },
+  ]);
+  const [selectedPlatForm, setSelectedPlatForm] = useState([
+    { name: "eBay", check: false },
+    { name: "Amazon", check: false },
+    { name: "Twitter", check: false },
+    { name: "Lazada", check: false },
+    { name: "Shopee", check: false },
+  ]);
+
   const [chartData, setChartData] = useState({
     labels: [
       "Boston",
@@ -40,53 +49,95 @@ const Statistic = () => {
     ],
   });
 
+  const menuCategory = (
+    <Menu>
+      <Menu.Item>
+        <Checkbox disabled> เลือกประเภทสินค้า (ไม่เกิน 3 ชนิด) </Checkbox>
+      </Menu.Item>
+      {selectedCat.map((item) => (
+        <Menu.Item key={item.name}>
+          <Checkbox
+            onClick={() => setShowCat(true)}
+            onChange={(e) => {
+              const newSelected: any = selectedCat.map(
+                (cat: { name: string; check: boolean }) => {
+                  return {
+                    name: cat.name,
+                    check:
+                      cat.name === item.name ? e.target.checked : cat.check,
+                  };
+                }
+              );
+              setSelectedCat(newSelected);
+            }}
+          >
+            {item.name}
+          </Checkbox>
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+
+  const menuPlatform = (
+    <Menu>
+      <Menu.Item>
+        <Checkbox disabled> เลือกแพลตฟอร์ม (ไม่เกิน 4 ชนิด) </Checkbox>
+      </Menu.Item>
+      {selectedPlatForm.map((item) => (
+        <Menu.Item key={item.name}>
+          <Checkbox
+            onChange={(e) => {
+              const newSelected: any = selectedPlatForm.map(
+                (cat: { name: string; check: boolean }) => {
+                  return {
+                    name: cat.name,
+                    check:
+                      cat.name === item.name ? e.target.checked : cat.check,
+                  };
+                }
+              );
+              setSelectedPlatForm(newSelected);
+            }}
+          >
+            {item.name}
+          </Checkbox>
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+
   return (
     <div>
       <Navbar name="Statistic" />
-      <Tabs size={"large"}>
-        <Tabs.TabPane tab="Graph" key="1">
+      <Tabs size={"large"} centered>
+        <Tabs.TabPane tab={<div>Graph</div>} key="1">
           <Chart title="Pongpang" chartData={chartData} />
-          <Divider></Divider>
-          <Row>
-            <Col span={6}></Col>
-            <Col span={6}>
-              <Dropdown
-                overlay={menuCategory}
-                trigger={["click"]}
-              >
-                <a
-                  className="ant-dropdown-link"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  Category <DownOutlined />
-                </a>
-              </Dropdown>
-            </Col>
-            <Col span={6}>
-              <Dropdown
-                overlay={menuPlatform}
-                trigger={["click"]}
-              >
-                <a
-                  className="ant-dropdown-link"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  Platform <DownOutlined />
-                </a>
-              </Dropdown>
-            </Col>
-            <Col span={6}></Col>
-          </Row>
+          <div className="flex justify-around items-center border-t border-gray-500 mt-10 h-12">
+            <Dropdown
+              visible={showCat}
+              onVisibleChange={setShowCat}
+              overlay={menuCategory}
+            >
+              <div>
+                Category <DownOutlined />
+              </div>
+            </Dropdown>
+            <Dropdown
+              visible={showPlatform}
+              overlay={menuPlatform}
+              onVisibleChange={setShowPlatform}
+            >
+              <div>
+                Platform <DownOutlined />
+              </div>
+            </Dropdown>
+          </div>
         </Tabs.TabPane>
         <Tabs.TabPane tab="Report" key="2"></Tabs.TabPane>
       </Tabs>
     </div>
   );
 };
-
-function onChange(e) {
-  console.log(`checked = ${e.target.checked}`);
-}
 
 const Chart = ({ chartData, title }) => {
   return (
@@ -108,52 +159,6 @@ const Chart = ({ chartData, title }) => {
   );
 };
 
-const menuCategory = (
-  <Menu>
-    <Menu.Item>
-      <Checkbox disabled> เลือกประเภทสินค้า (ไม่เกิน 3 ชนิด) </Checkbox>
-    </Menu.Item>
-    <Menu.Item>
-      <Checkbox onChange={onChange}> เครื่องสำอาง </Checkbox>
-    </Menu.Item>
-    <Menu.Item>
-      <Checkbox onChange={onChange}> เสื้อผ้า </Checkbox>
-    </Menu.Item>
-    <Menu.Item>
-      <Checkbox onChange={onChange}> เครื่องใช้ไฟฟ้า </Checkbox>
-    </Menu.Item>
-    <Menu.Item>
-      <Checkbox onChange={onChange}> ของใช้ในครัว </Checkbox>
-    </Menu.Item>
-    <Menu.Item>
-      <Checkbox onChange={onChange}> ของใช้ในสำนักงาน </Checkbox>
-    </Menu.Item>
-  </Menu>
-);
-
-const menuPlatform = (
-  <Menu>
-    <Menu.Item>
-      <Checkbox disabled> เลือกแพลตฟอร์ม (ไม่เกิน 4 ชนิด) </Checkbox>
-    </Menu.Item>
-    <Menu.Item>
-      <Checkbox onChange={onChange}> eBay </Checkbox>
-    </Menu.Item>
-    <Menu.Item>
-      <Checkbox onChange={onChange}> Amazon </Checkbox>
-    </Menu.Item>
-    <Menu.Item>
-      <Checkbox onChange={onChange}> Twitter </Checkbox>
-    </Menu.Item>
-    <Menu.Item>
-      <Checkbox onChange={onChange}> Lazada </Checkbox>
-    </Menu.Item>
-    <Menu.Item>
-      <Checkbox onChange={onChange}> Shopee </Checkbox>
-    </Menu.Item>
-  </Menu>
-);
-
-const { Title } = Typography;
+const Graph = () => {};
 
 export default Statistic;
