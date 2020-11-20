5864ee1d2df0c5677857cfea69fea0815168eefb
inregist <inregist.irg@gmail.com>
2020-11-20 14:13:57 +0700

    added formatinput

--- office/src/pages/Main.tsx
+++ office/src/pages/Main.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { formatInput } from "../components/utils";
 
 const Main = () => {
-    return (
-        <div style={{background: "#fbfbfb"}} className="h-screen">
-            Hello US
-        </div>
-    );
-} 
+  return (
+    <div style={{ background: "#fbfbfb" }} className="h-screen p-24">
+      <input
+        type="text"
+        onChange={(e) => formatInput(e, "xxx-xxx-xxxx")}
+        className="border border-black"
+      />
+    </div>
+  );
+};
 
 export default Main;
