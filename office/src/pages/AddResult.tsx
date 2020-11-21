70f65fab9d22acaac8c28ddc1f149bd2784cb9bc
inregist <inregist.irg@gmail.com>
2020-11-21 16:40:30 +0700

    datepicker from antd

--- office/src/pages/AddResult.tsx
+++ office/src/pages/AddResult.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal, Input} from "antd";
+import { Modal, Input } from "antd";
 import Garbage from "../assets/garbage.png";
 const AddResult = () => {
   const { TextArea } = Input;
@@ -10,7 +10,9 @@ const AddResult = () => {
       cancelButtonProps={{ style: { display: "none" } }}
       okButtonProps={{ style: { display: "none" } }}
     >
-      <div className=" p-4 text-xl font-bold">การไกล่เกลี่ย : C0020 ครั้งที่ 2</div>
+      <div className=" p-4 text-xl font-bold">
+        การไกล่เกลี่ย : C0020 ครั้งที่ 2
+      </div>
       <div className="flex  border-t border-gray-500 pt-8 pb-4 pl-4 pr-4 font-bold">
         เวลานัดไกล่เกลี่ย
       </div>
@@ -19,18 +21,18 @@ const AddResult = () => {
       <TextArea rows={5} />
       <div className="pt-12 pl-4 flex ">
         <div className="pr-12">
-          <button className="rounded-lg bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded w-32">
+          <button className="rounded-lg bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 w-32">
             บันทึก
           </button>
         </div>
         <div className="pr-64">
-          <button className="rounded-lg bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded w-32">
+          <button className="rounded-lg bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 w-32">
             ยกเลิก
           </button>
         </div>
         <div className="pl-32">
           <button className="inline-flex items-center justify-center w-10 h-10 mr-2  transition-colors duration-150 bg-red-700 rounded-lg focus:shadow-outline ">
-                <img src={Garbage} width="20" height="20" alt="icon" />
+            <img src={Garbage} width="20" height="20" alt="icon" />
           </button>
         </div>
       </div>
--- office/src/pages/AddTime.tsx
+++ office/src/pages/AddTime.tsx
@@ -1,30 +1,22 @@
-import React,{useState} from "react";
-import { Modal } from "antd";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import React, { useState } from "react";
+import { Modal, DatePicker } from "antd";
+// import DatePicker from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
 
 const AddTime = () => {
   const [show, setShow] = useState(true);
-  const [startDate, setStartDate] = useState(new Date());
   return (
     <Modal
       width={320}
       visible={show}
-      cancelButtonProps={{ style: { display: 'none' } }}
-      okButtonProps={{ style: { display: 'none' } }}
+      cancelButtonProps={{ style: { display: "none" } }}
+      okButtonProps={{ style: { display: "none" } }}
     >
-      <div className="md:font-bold  h-10 text-lg flex w-full flex justify-around  border-b border-gray-500 p-0.5">
+      <div className="md:font-bold  h-10 text-lg w-full flex justify-around  border-b border-gray-500 p-0.5">
         เพิ่มเวลานัดหมาย
       </div>
       <div className="pt-12 pb-4 pl-10 pr-4 flex justify-center ">
-        <DatePicker
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-          timeInputLabel="Time:"
-          dateFormat="MM/dd/yyyy h:mm aa"
-          showTimeInput
-          
-        />
+        <DatePicker showTime={{ format: "HH:mm" }} />
       </div>
 
       <div className=" flex justify-center pt-8 pb-px pl-4 pr-4">
