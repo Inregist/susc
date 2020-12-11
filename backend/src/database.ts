e809a0eadc73e5537ea79e43320519b817d614a7
inregist <inregist.irg@gmail.com>
2020-12-11 15:49:56 +0700

    delete backend

--- backend/src/database.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from 'pg';
-
-const connectionString = process.env.DB_CONNECT || "postgresql://root:root@localhost:5432/susc"
-
-const pool = new Pool({ connectionString })
-
-export const query = async (text, value=[]) => {
-  return await pool.query(text,value)
-}
--- backend/src/types/case.d.ts
+++
@@ -1,3 +0,0 @@
-type Case ={
-  id: number,
-}
